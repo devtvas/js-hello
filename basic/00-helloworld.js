51b05c89147f74ad01687e2f62c93317565ac04e
devtvas <tarcisio.word@gmail.com>
2024-07-24 18:27:51 -0300

    basic - hello world

---
+++ basic/00-helloworld.js
@@ -0,0 +1,16 @@
+/*
+Clase 1 en vídeo | 10/07/2024
+Introducción, instalación, hola mundo y variables
+https://www.twitch.tv/videos/2194200202?t=00h14m43s
+*/
+
+// Esto es un comentario simple
+
+/*
+Esto es
+un comentario
+en varias líneas
+*/
+
+console.log('Hola, JavaScript');
+
