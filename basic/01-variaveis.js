f5fc4e7e8f28a523e432af797e4c298d38bc44b2
devtvas <tarcisio.word@gmail.com>
2024-07-25 14:10:42 -0300

    basic - variaveis

---
+++ basic/01-variaveis.js
@@ -0,0 +1,14 @@
+//var
+
+var helloWorld =  "Ola, JavaScript com Var!"
+console.log(helloWorld);
+
+// let
+
+let helloWorld2 = "Ola, JavaScript com Let!"
+console.log(helloWorld2);
+
+// const
+
+const helloWorld3 = "Ola, JavaScript com Const!"
+console.log(helloWorld3);
